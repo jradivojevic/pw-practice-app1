94548f93fda87af29c69667d9a8c031cb5e26ff8
jradivojevic <radivojevicjelena@outlook.com>
2024-09-29 15:07:45 -0400

    locating child elements

--- tests/secondTest.Spec.ts
+++ tests/secondTest.Spec.ts
@@ -40,3 +40,28 @@ test('Locator syntax rules', async({page}) => {
 })
 
 
+test('User facing locators', async({page}) =>{
+    await page.getByRole('textbox', {name: "Email"}).first().click()
+    await page.getByRole('button', {name: 'Sign in'}).first().click()
+    await page.getByLabel('Email').first().click()
+    await page.getByPlaceholder('Jane Doe')
+    await page.getByText('Using the Grid').click()
+   // await page.getByTitle('IoT Dashboard').click
+
+    await page.getByTestId('SignIn').click()
+
+})
+
+test('locating child elements', async({page}) =>{
+await page.locator('nb-card nb-radio :text-is("Option 1")').click()
+await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click()
+await page.locator('nb-card').getByRole('button', {name:"Sign in"}).first().click()
+
+await page.locator('nb-card').nth(3).getByRole('button').click
+
+
+
+})
+
+
+
